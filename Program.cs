915413c4216f258f3505cb5baad6ce7f46fbc901
Darshit Shah <Darshit.theanushwara@gmail.com>
2024-12-01 21:33:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeedyAir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
